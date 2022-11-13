46138f1c8dabe2e8608d63efd4aa9c1fdce6d37a
Jessica Oliveira <jessica.leiteandre@gmail.com>
2022-11-13 18:04:46 -0300

    atualização 3

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import config from "../config.json"
 import styled from "styled-components"
-import { CSSReset } from "../src/components/CSSReset"
 import Menu from "../src/components/Menu"
 import { StyledTimeline } from "../src/components/Timeline"
+
 
 function HomePage() {
 
@@ -12,7 +12,6 @@ function HomePage() {
     return (
       <>
 
-      <CSSReset />
     <div style={{
                 display: "flex",
                 flexDirection: "column",
@@ -38,6 +37,7 @@ function HomePage() {
 `;
 
 const StyledHeader = styled.div`
+   background-color: ${({ theme }) => theme.backgroundLevel1};
    img {
       width: 80px;
       height: 80px;
